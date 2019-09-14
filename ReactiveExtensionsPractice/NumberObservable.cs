2a836945ff9d70a7f6f6cb157daba104a7419ee4
shuhey <nishizawa@timedia.co.jp>
2019-09-14 11:35:36 +0900

    Add project files.

---
+++ ReactiveExtensionsPractice/NumberObservable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReactiveExtensionsPractice
+{
+    class NumberObservable : IObservable<int>
+    {
+        //自分を監視している人を管理するリスト
+        public List<IObserver<int>> observers = new List<IObserver<int>>();
+
+        public void Execute(int value)
+        {
+            if (value == 0)
+            {
+                foreach (var obs in observers)
+                {
+                    obs.OnError(new Exception("value is 0"));
+                }
+
+                //エラーが起きたので処理は終了
+                this.observers.Clear();
+
+                return;
+            }
+
+            foreach (var obs in observers)
+            {
+                obs.OnNext(value);
+            }
+        }
+
+        //完了通知
+        public void Completed()
+        {
+            foreach (var obs in observers)
+            {
+                obs.OnCompleted();
+            }
+
+            //完了したので監視している人たちをクリア
+            this.observers.Clear();
+        }
+
+       　//監視する人を追加する
+        //戻り値のIDisposableをDisposeすると監視から外れる
+        public IDisposable Subscribe(IObserver<int> observer)
+        {
+            this.observers.Add(observer);
+            return new RemoveListDisosable(observers, observer);
+        }
+    }
+}
---
+++ ReactiveExtensionsPractice/RemoveListDisosable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReactiveExtensionsPractice
+{
+    //Disposeが呼ばれたらobserverを監視対象から削除する
+    internal class RemoveListDisosable : IDisposable
+    {
+        private List<IObserver<int>> observers;
+        private IObserver<int> observer;
+
+        public RemoveListDisosable(List<IObserver<int>> observers, IObserver<int> observer)
+        {
+            this.observers = observers;
+            this.observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (this.observers == null)
+            {
+                return;
+            }
+
+            if (observers.IndexOf(observer) != -1)
+            {
+                this.observers.Remove(observer);
+            }
+
+            this.observers = null;
+            this.observer = null;
+        }
+    }
+}
