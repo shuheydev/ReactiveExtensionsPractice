f1e0b6eb488704b7fb1f570083f91db7de1d98c1
shuhey <nishizawa@timedia.co.jp>
2019-09-15 16:48:41 +0900

    Throttle

--- ReactiveExtensionsPractice/Program.cs
+++ ReactiveExtensionsPractice/Program.cs
@@ -12,130 +12,39 @@ namespace ReactiveExtensionsPractice
     {
         static void Main(string[] args)
         {
-            Observable.Range(1, 10)
-                //3つずつの値に分ける
-                .Buffer(3)
-                .Subscribe(
-                    list =>
-                    {
-                        //IList<int>の内容を出力
-                        Console.WriteLine("-- Buffer start");
-                        foreach (var i in list)
-                        {
-                            Console.WriteLine(i);
-                        }
-                    },
-                    //完了
-                    () => Console.WriteLine("OnCompleted")
-                );
+            var source = new Subject<int>();
 
-            Observable.Range(1, 10)
-                //3つずつの値に分けて、値は2つ飛ばし
-                .Buffer(3, 2)
-                .Subscribe(
-                    list =>
-                    {
-                        //IList<int>の内容を出力
-                        Console.WriteLine("-- Buffer start");
-                        foreach (var i in list)
-                        {
-                            Console.WriteLine(i);
-                        }
-                    },
-                    //完了
-                    () => Console.WriteLine("OnCompleted")
-                );
+            source
+                .Throttle(TimeSpan.FromMilliseconds(500))
+                //渡ってきた値を時間付きで表示
+                .Subscribe(i =>
+                    Console.WriteLine("{0:HH:mm:ss.fff} {1}", DateTime.Now, i));
 
-            Observable.Range(1, 10)
-                //3つずつの値に分けて、値は2つ飛ばし
-                .Buffer(3, 5)
-                .Subscribe(
-                    list =>
-                    {
-                        //IList<int>の内容を出力
-                        Console.WriteLine("-- Buffer start");
-                        foreach (var i in list)
-                        {
-                            Console.WriteLine(i);
-                        }
-                    },
-                    //完了
-                    () => Console.WriteLine("OnCompleted")
-                );
+            //100ms間隔で値を発行
+            foreach (var i in Enumerable.Range(1, 10))
+            {
+                //発行した値を出力しておく
+                Console.WriteLine("{0:HH:mm:ss.fff} OnNext({1})", DateTime.Now, i);
+                source.OnNext(i);
+                Thread.Sleep(100);
+            }
 
-            var gate = new EventWaitHandle(false, EventResetMode.AutoReset);
-            Observable
-                //500msごとに値を発行
-                .Interval(TimeSpan.FromMilliseconds(500))
-                //3秒間溜める
-                .Buffer(TimeSpan.FromSeconds(3))
-                //最初の3つのかたまりを後続に流す
-                .Take(3)
-                .Subscribe(
-                    l =>
-                    {
-                        // 値を表示
-                        Console.WriteLine("--Buffer {0:HH:mm:ss}", DateTime.Now);
-                        foreach (var i in l)
-                        {
-                            Console.WriteLine(i);
-                        }
-                    },
-                    () =>
-                    {
-                        // 完了
-                        Console.WriteLine("OnCompleted");
-                        gate.Set();
-                    });
+            // 2000ms sleep
+            Console.WriteLine("{0:HH:mm:ss.fff} Sleep(2000)", DateTime.Now);
+            Thread.Sleep(2000);
 
-            // OnCompleted待ち
-            Console.WriteLine("WaitOne");
-            gate.WaitOne();
-            Console.WriteLine("WaitOne Completed");
+            // 100ms間隔で値を発行
+            foreach (var i in Enumerable.Range(1, 5))
+            {
+                // 発行した値を出力しておく
+                Console.WriteLine("{0:HH:mm:ss.fff} OnNext({1})", DateTime.Now, i);
+                source.OnNext(i);
+                Thread.Sleep(100);
+            }
 
-            gate = new EventWaitHandle(false, EventResetMode.AutoReset);
-            var clickEmuration = new Subject<Unit>();
-            Observable
-                //500ms間隔で値を発行
-                .Interval(TimeSpan.FromMilliseconds(500))
-                //任意の値で値をまとめるのをやめる（この場合3秒間隔）
-                .Buffer(
-                    //clickEmurationから通知が来たら
-                    clickEmuration.AsObservable(),
-                    //2秒間値を集める
-                    _ => Observable.Interval(TimeSpan.FromSeconds(2))
-                )
-                .Take(2)
-                .Subscribe(
-                    list =>
-                    {
-                        //値を表示
-                        Console.WriteLine($"--Buffer {DateTime.Now.ToString("0:HH:mm:ss")}");
-                        foreach (var i in list)
-                        {
-                            Console.WriteLine(i);
-                        }
-                    },
-                    () =>
-                    {
-                        //完了
-                        Console.WriteLine("OnCompleted");
-                        gate.Set();
-                    }
-                );
-
-            Console.ReadLine();
-            Console.WriteLine("{0:HH:mm:ss} Click emurate", DateTime.Now);
-            clickEmuration.OnNext(Unit.Default);
-
-            //Enterを押すとクリックを模したSubjectから通知を上げる
-            Console.ReadLine();
-            Console.WriteLine("{0:HH:mm:ss} Click emurate", DateTime.Now);
-            clickEmuration.OnNext(Unit.Default);
-
-            //OnCompleted待ち
-            gate.WaitOne();
-            Console.WriteLine("WaiteOne Completed");
+            // 2000ms sleep
+            Console.WriteLine("{0:HH:mm:ss.FFF} Sleep(2000)", DateTime.Now);
+            Thread.Sleep(2000);
         }
     }
 }
